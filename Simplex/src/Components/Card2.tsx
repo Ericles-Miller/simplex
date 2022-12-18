c65fdff4bf2d9472310aae57d25451fa8d78f9b8
Éricles Miller <ericlesmiller15@gmail.com>
2022-12-18 16:19:46 -0300

    part1 ready

--- Simplex/src/Components/Card2.tsx
+++ Simplex/src/Components/Card2.tsx
@@ -1,24 +1,26 @@
 import { Card, CardActionArea, CardActions, CardContent, Typography } from "@mui/material";
 import styles from './Card.module.css';
 import { Constraints } from "./Constraints";
+import { GenerateProblem } from "./GenerateProblem";
 
-export function Card2(){
-    return (
-        <Card className={styles.card}>
+export function Card2({ children }) {
+  return (
+    <Card className={styles.card}>
       <CardActionArea>
-        
+
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             Gerar Problema
           </Typography>
-          <Typography variant="body2">   
-            Coloque o numero de variaveis de decisao e o numero de restricoes. Alem disso selecione 
+          <Typography variant="body2">
+            Coloque o numero de variaveis de decisao e o numero de restricoes. Alem disso selecione
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Constraints></Constraints>
+
+      <CardActions style={{display:'block'}}>
+        {children}
       </CardActions>
     </Card>
-    )
+  )
 }
--- Simplex/src/Components/GenerateProblem.tsx
+++ Simplex/src/Components/GenerateProblem.tsx
@@ -1,10 +1,83 @@
+import { FormEvent, useState } from "react";
 import { Card2 } from "./Card2";
+import { useContentxProblem } from "../context/ProblemContentex";
+import { TextField } from "@mui/material";
+
+
+
+interface IDataProblem {
+  numberVariable: number,
+  numberConstraints: number,
+  method: string,
+  option: string
+}
 
 
 export function GenerateProblem() {
-    return (
-      <div>
-        <Card2 />
-      </div>
-    ); 
+  const object = useContentxProblem()
+  console.log(object.data)
+
+  return (
+    <div>
+      <Card2 >
+        <div style={{ display: 'flex' }}>
+          {Array.from({
+            length: object.data.numberConstraints
+          }).map((item, index) => {
+
+            return <div>
+              <TextField
+                name="InputRestrictions"
+                id="InputRestrictions"
+                label={`x${index + 1}`}
+                type='number'
+                color="success"
+                focused
+              />
+              {index != object.data.numberConstraints - 1 ? <label >+</label> : null}
+            </div>
+          })}
+        </div>
+
+
+        <div >
+          {Array.from({
+            length: object.data.numberVariable,
+
+          }).map(() => {
+            return (
+              <div style={{ display: 'flex' }}>
+                {Array.from({
+                  length: object.data.numberConstraints,
+                }).map((item, column) => {
+                  return <div><TextField
+                    name="InputRestrictions"
+                    id="InputRestrictions"
+                    label={`x${column + 1}`}
+                    type='number'
+                    color="success"
+                    focused
+                  />
+                    {column != object.data.numberConstraints - 1 ? <label >+</label> : null}
+                  </div>
+                })}
+                <select name="" id="">
+                  <option>{'<='}</option>
+                  <option>{'='}</option>
+                  <option>{'=>'}</option>
+                </select>
+                <TextField
+                  name="InputRestrictions"
+                  id="InputRestrictions"
+                  type='number'
+                  color="success"
+                  focused
+                />
+              </div>
+            )
+          })}
+        </div>
+      </Card2>
+    </div>
+  )
 }
