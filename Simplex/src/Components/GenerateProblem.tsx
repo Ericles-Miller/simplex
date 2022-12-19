73acf11673c20cde62ec49e7663d385b89a9ec03
Ericles-Miller <ericlesmiller15@gmail.com>
2022-12-19 15:38:34 -0300

    read Json initial

--- Simplex/src/Components/GenerateProblem.tsx
+++ Simplex/src/Components/GenerateProblem.tsx
@@ -15,26 +15,26 @@ export function GenerateProblem() {
   const object = useContextProblem()
   console.log(object.data, 'aaaaa')
 
-  const [result, setResult ] = useState({
+  const [result, setResult] = useState({
     constraintsMethod: {
 
     },
     numberVariablesMethod: Array.from({
-      length:object.data.numberVariable
-    }).map(()=>{}),
+      length: object.data.numberVariable
+    }).map(() => { }),
     method: object.data.method,
     option: object.data.option,
     type: object.data.type
   })
 
-  function HandleChangesVariable(row:any, item:any, value:any,) {
-    setResult((previousState)=>{
+  function HandleChangesVariable(row: any, item: any, value: any,) {
+    setResult((previousState) => {
       return {
         ...previousState,
-        numberVariablesMethod: previousState.numberVariablesMethod.map((original, index)=>{
-          if(index == row){
+        numberVariablesMethod: previousState.numberVariablesMethod.map((original, index) => {
+          if (index == row) {
             return {
-              ...original, 
+              ...original,
               [item]: value,
             }
           }
@@ -43,20 +43,20 @@ export function GenerateProblem() {
       }
     })
   }
-  
-  function HandleChangesConstraints(item:any,value:any){
-    setResult((previousState)=>{
+
+  function HandleChangesConstraints(item: any, value: any) {
+    setResult((previousState) => {
       return {
         ...previousState,
-        constraintsMethod:{
-          ...previousState.constraintsMethod, 
-          [item]:value
+        constraintsMethod: {
+          ...previousState.constraintsMethod,
+          [item]: value
         }
       }
     })
   }
 
-  function handleSubmit(){
+  function handleSubmit() {
     const link = document.createElement('a')
 
     link.download = `data.json`
@@ -74,83 +74,83 @@ export function GenerateProblem() {
   return (
     <div>
       <form >
-      <Card2 >
-        
-        <div style={{ display: 'flex', marginBottom: '1.5rem', justifyContent: 'center', alignItems: "center"}}>
-        <strong style={{paddingRight: "1rem"}}>Função</strong>
-          {Array.from({
-            length: object.data.numberConstraints
-          }).map((item, index) => {
-            
-            return <div style={{display: 'flex', alignItems: 'center'}}>
-              <TextField
-                style={{ marginLeft: '0.5rem' }}
-                name="InputRestrictions"
-                id="InputRestrictions"
-                label={`x${index + 1}`}
-                type='number'
-                color="success"
-                focused
-                onChange={(event)=>HandleChangesConstraints(`x${index + 1}`,event.target.value )}
-                required
-              />
-              {index != object.data.numberConstraints - 1 ? <strong style={{ marginLeft: '0.5rem' }}>+</strong> : null}
-            </div>
-          })}
-        </div>
+        <Card2 >
 
+          <div style={{ display: 'flex', marginBottom: '1.5rem', justifyContent: 'center', alignItems: "center" }}>
+            <strong style={{ paddingRight: "1rem" }}>Função</strong>
+            {Array.from({
+              length: object.data.numberConstraints
+            }).map((item, index) => {
 
-        <div>
-          {Array.from({
-            length: object.data.numberVariable,
-
-          }).map((_,row) => {
-            return (
-              <div style={{ display: 'flex', marginBottom: '1.5rem' }}>
-                {Array.from({
-                  length: object.data.numberConstraints,
-                }).map((item, column) => {
-                  return <div style={{display: 'flex', alignItems: 'center'}}>
-                    <TextField
-                    name="InputRestrictions"
-                    id="InputRestrictions"
-                    label={`x${column + 1}`}
-                    type='number'
-                    color="success"
-                    focused
-                    onChange={(event)=>HandleChangesVariable(row,`x${column + 1}`, event.target.value)}
-                    required
-                  />
-                    {column != object.data.numberConstraints - 1 ? <strong style={{ marginLeft: '0.5rem', marginRight: '0.5rem' }}>+</strong> : null}
-                  </div>
-                })}
-                <select name="" id=""
-                  onChange={(event)=>HandleChangesVariable(row,'simbol', event.target.value)}
-                  style={{ marginLeft: '0.5rem', marginRight: '0.5rem' }}
-                >
-                  <option value='<='>{'<='}</option>
-                  <option value='='>{'='}</option>
-                  <option value='=>'>{'=>'}</option>
-                </select>
+              return <div style={{ display: 'flex', alignItems: 'center' }}>
                 <TextField
+                  style={{ marginLeft: '0.5rem' }}
                   name="InputRestrictions"
                   id="InputRestrictions"
+                  label={`x${index + 1}`}
                   type='number'
                   color="success"
                   focused
-                  onChange={(event)=>HandleChangesVariable(row,'result', event.target.value)}
+                  onChange={(event) => HandleChangesConstraints(`x${index + 1}`, event.target.value)}
                   required
                 />
+                {index != object.data.numberConstraints - 1 ? <strong style={{ marginLeft: '0.5rem' }}>+</strong> : null}
               </div>
-            )
-          })}
-        </div>
-        <div style={{marginTop: '2rem'}}>
-          <Button onClick={handleSubmit} type="submit"  variant="contained" color="success">
-            Enviar Dados
-          </Button>
-        </div>
-      </Card2>
+            })}
+          </div>
+
+
+          <div>
+            {Array.from({
+              length: object.data.numberVariable,
+
+            }).map((_, row) => {
+              return (
+                <div style={{ display: 'flex', marginBottom: '1.5rem' }}>
+                  {Array.from({
+                    length: object.data.numberConstraints,
+                  }).map((item, column) => {
+                    return <div style={{ display: 'flex', alignItems: 'center' }}>
+                      <TextField
+                        name="InputRestrictions"
+                        id="InputRestrictions"
+                        label={`x${column + 1}`}
+                        type='number'
+                        color="success"
+                        focused
+                        onChange={(event) => HandleChangesVariable(row, `x${column + 1}`, event.target.value)}
+                        required
+                      />
+                      {column != object.data.numberConstraints - 1 ? <strong style={{ marginLeft: '0.5rem', marginRight: '0.5rem' }}>+</strong> : null}
+                    </div>
+                  })}
+                  <select name="" id=""
+                    onChange={(event) => HandleChangesVariable(row, 'simbol', event.target.value)}
+                    style={{ marginLeft: '0.5rem', marginRight: '0.5rem' }}
+                  >
+                    <option value='<='>{'<='}</option>
+                    <option value='='>{'='}</option>
+                    <option value='=>'>{'=>'}</option>
+                  </select>
+                  <TextField
+                    name="InputRestrictions"
+                    id="InputRestrictions"
+                    type='number'
+                    color="success"
+                    focused
+                    onChange={(event) => HandleChangesVariable(row, 'result', event.target.value)}
+                    required
+                  />
+                </div>
+              )
+            })}
+          </div>
+          <div style={{ marginTop: '2rem' }}>
+            <Button onClick={handleSubmit} type="submit" variant="contained" color="success">
+              Enviar Dados
+            </Button>
+          </div>
+        </Card2>
       </form>
     </div>
   )
--- Simplex/src/Components/PivotArray.tsx
+++ Simplex/src/Components/PivotArray.tsx
@@ -1,15 +1,27 @@
-
-export async function PivotArray() {
-  const response = await fetch('./../../Data.json');
-  const data = await response.json();
-  
-  
+import React, { useState, useEffect } from "react"
+import { Card2 } from "./Card2";
 
 
+export  function PivotArray() {
 
+  const [receivedData, newReceivedData] = useState({} as any)
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch('./../../Data.json');
+      const data = await response.json();
+      newReceivedData(data);
+    }
+    getData();
+  }, []);
+
+  console.log(receivedData)
 
 
   return (
-    <p>asssss</p>
+    <div>
+      <Card2></Card2>
+    </div>
   )
 }
+
