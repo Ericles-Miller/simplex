d5001f5dfa2b1697a2a9dcc224b5fcd4b6b2d441
Éricles Miller <ericlesmiller15@gmail.com>
2022-12-19 23:47:22 -0300

    send dados.json

--- Simplex/src/Components/PivotArray.tsx
+++ Simplex/src/Components/PivotArray.tsx
@@ -2,25 +2,33 @@ import React, { useState, useEffect } from "react"
 import { Card2 } from "./Card2";
 
 
+
+
 export  function PivotArray() {
 
   const [receivedData, newReceivedData] = useState({} as any)
 
   useEffect(() => {
     const getData = async () => {
-      const response = await fetch('./../../Data.json');
+      const response = await fetch('./../../dados.json');
       const data = await response.json();
       newReceivedData(data);
     }
     getData();
   }, []);
 
+
   console.log(receivedData)
+
+
+
 
 
   return (
     <div>
-      <Card2></Card2>
+      <Card2>
+        
+      </Card2>
     </div>
   )
 }
