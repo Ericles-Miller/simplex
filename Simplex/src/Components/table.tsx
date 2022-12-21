79701a53cea6ab96d3b6916941d34867baaae238
Ericles-Miller <ericlesmiller15@gmail.com>
2022-12-21 11:19:10 -0300

    insert table ready

--- Simplex/src/Components/table.tsx
+++ Simplex/src/Components/table.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './table.module.css'
 import { object } from 'prop-types';
 
@@ -6,8 +6,12 @@ import { object } from 'prop-types';
 
 export function Table() {
   const [receivedData, setReceivedData] = useState<object[]>()
-  const [nicePoint, setNicePoint]       = useState<object[]>()
-  
+  const [nicePoint, setNicePoint] = useState<object[]>()
+  const pivot: Array<number> = []
+  //#F2E399
+  const [backgroundColorPivot, setBackgroundColorPivot] = useState('')
+  const [colorPivot, setColorPivot] = useState('')
+
   const getData = async () => {
     const response = await fetch('./../../result.json');
     const data = await response.json();
@@ -25,70 +29,88 @@ export function Table() {
     getDataNicePoint();
   }, []);
 
-  console.log(nicePoint)
 
   return (
     <>
-    <div>
-      <div className={styles.divText}>
-        <strong style={{ color: '#1C724B' }}>Ponto Ótimo:</strong>
-        { nicePoint != undefined &&  nicePoint.map((item:any)=>(
+      <div>
+        <div className={styles.divText}>
+          <strong style={{ color: '#1C724B' }}>Ponto Ótimo:</strong>
+          {nicePoint != undefined && nicePoint.map((item: any) => (
             <label >Z:{item.solution.Z} X1:{item.solution.X1} X2:{item.solution.X2} X3:{item.solution.X3} </label>
           ))
-        }
-      </div>
-      <div className={styles.divText}>
-        <strong style={{ color: '#1C724B' }}>Valor Otimo: </strong>
-        <label >numero qualquer</label>
-      </div>
-      <div className={styles.goodPoint}>
-        <strong>Nao foi possivel encontrar a solucao inteira </strong>
-      </div>
-      { receivedData != undefined && receivedData.map((rd: any)=> (
-        <p>{rd.erro}</p>
-      ))}
-
-      <div>
-      {receivedData != undefined && receivedData.map((rd: any)=> (
-        <div className={styles.tableContainer}>
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th>BASE</th>
-              <th>z</th>
-              {
-                (rd.variable).map((item: string) =>(  
-                  <th>{item}</th>
-                ))
-              }
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-                <td>Z</td>
-                {
-                  (rd.z).map((item: string) =>(
-                      <td>{item}</td>
-                  ))
-                }
-            </tr>
-
-          {
-            (rd.base).map((item: string, index:number) =>(  
-                <tr>
-                  <td>{item}</td>
-                  {
-                    rd.table[index].map((subItem: any) => <td>{subItem}</td>)
-                  }
-                </tr>
-            ))
           }
-          </tbody>
-        </table>
+        </div>
+        <div className={styles.divText}>
+          <strong style={{ color: '#1C724B' }}>Valor Otimo: </strong>
+          <label >numero qualquer</label>
+        </div>
+        <div className={styles.goodPoint}>
+          <strong>Nao foi possivel encontrar a solucao inteira </strong>
+        </div>
+        {receivedData != undefined && receivedData.map((rd: any) => (
+          <p>{rd.erro}</p>
+        ))}
+
+        {receivedData != undefined && receivedData.map((rd: any) => (
+          pivot.push(rd.pivo)
+        ))}
+
+        {console.log(pivot)}
+
+
+        <div>
+          {receivedData != undefined && receivedData.map((rd: any) => (
+            <div className={styles.tableContainer}>
+              <table className={styles.table}>
+                <thead>
+                  <td>Table 1</td>
+                  <td></td>
+                  <td></td>
+                  {
+                    (rd.z).map((item: string) => (
+                      <td>{item}</td>
+                    ))
+                  }
+                  <tr>
+                    <th>BASE</th>
+
+                    {
+                      (rd.variable).map((item: string) => (
+                        <th>{item}</th>
+                      ))
+                    }
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+
+                  </tr>
+
+                  {
+                    (rd.base).map((item: string, index: number) => (
+                      <tr>
+                        <td>{item}</td>
+                        {
+                          rd.table[index].map((subItem: any) =>
+                            <td style={{backgroundColor: `${backgroundColorPivot}`, color: `${colorPivot}`}}>{subItem}</td>)
+                        }
+
+                      </tr>
+                    ))
+                  }
+                  <td>Z</td>
+                  <td></td>
+                  {
+                    rd.table[rd.table.length - 1].map((value: any) => (
+                      <td>{value}</td>
+                    ))
+                  }
+                </tbody>
+              </table>
+            </div>
+          ))}
+        </div>
       </div>
-      ))}
-    </div>
-    </div>
     </>
   );
 }
